a90a14655671a340e54f2299f633f746abca9d93
Mahesh <mahesh001.mg@gmail.com>
2024-05-18 16:57:56 +0530

    Login/SignUp Added

--- src/App.js
+++ src/App.js
@@ -1,46 +1,67 @@
-import React from 'react';
-import Counter from './features/counter/Counter';
-import './App.css';
-import ProductList from './features/product/components/ProductList';
-import Home from './pages/Home';
-import Login from './features/auth/component/Login';
-import LoginPage from './pages/LoginPage';
-import SignupPage from './pages/SignupPage';
-import CartPage from './pages/Cart';
-import CheckoutPage from './pages/Checkout';
-import ProductDetails from "./features/product/components/ProductDetails"
-import ProductDetailPage from './pages/ProductDetailsPage';
+import React from "react";
+import Counter from "./features/counter/Counter";
+import "./App.css";
+import ProductList from "./features/product/components/ProductList";
+import Home from "./pages/Home";
+import Login from "./features/auth/component/Login";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import CartPage from "./pages/Cart";
+import CheckoutPage from "./pages/Checkout";
+import ProductDetails from "./features/product/components/ProductDetails";
+import ProductDetailPage from "./pages/ProductDetailsPage";
 import {
   createBrowserRouter,
   RouterProvider,
   Route,
   Link,
 } from "react-router-dom";
+import Protected from "./features/auth/component/Protected";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: (<Home></Home>),
+    element: (
+      <Protected>
+        {" "}
+        <Home></Home>
+      </Protected>
+    ),
   },
   {
     path: "/login",
-    element: (<LoginPage></LoginPage>),
+    element: <LoginPage></LoginPage>,
   },
   {
     path: "/signup",
-    element: (<SignupPage></SignupPage>),
+    element: <SignupPage></SignupPage>,
   },
   {
     path: "/cart",
-    element: (<CartPage></CartPage>),
+    element: (
+      <Protected>
+        {" "}
+        <CartPage></CartPage>
+      </Protected>
+    ),
   },
   {
     path: "/checkout",
-    element: (<CheckoutPage></CheckoutPage>)
+    element: (
+      <Protected>
+        {" "}
+        <CheckoutPage></CheckoutPage>
+      </Protected>
+    ),
   },
   {
     path: "/product-detail/:id",
-    element: (<ProductDetailPage></ProductDetailPage>)
+    element: (
+      <Protected>
+        {" "}
+        <ProductDetailPage></ProductDetailPage>
+      </Protected>
+    ),
   },
 ]);
 
--- src/features/auth/authAPI.js
+++ src/features/auth/authAPI.js
@@ -1,8 +1,30 @@
-export function fetchCount(amount = 1) {
+export function createUser(userData) {
   return new Promise(async (resolve) => {
-    const response = await fetch("http://localhost:8000")
-    const data = await response.json()
-    resolve({ data })
-  }
-  )
+    const response = await fetch("http://localhost:8080/users", {
+      method: "POST",
+      body: JSON.stringify(userData),
+      headers: { "content-type": "application/json" },
+    });
+    const data = await response.json();
+    resolve({ data });
+  });
+}
+
+export function checkUser(loginInfo) {
+  return new Promise(async (resolve, reject) => {
+    const email = loginInfo.email;
+    const password = loginInfo.password;
+    const response = await fetch("http://localhost:8080/users?email=" + email);
+    const data = await response.json();
+    console.log({data})
+    if (data.length) {
+      if (password === data[0].password) {
+        resolve({ data: data[0] });
+      } else {
+        reject({ message: "Wrong credentials" });
+      }
+    } else {
+      reject({ message: "user not found" });
+    }
+  });
 }
